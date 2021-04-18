6cfa24271e94a28f69b6525aef95205c9cfe28bf
RidwanMalik <skredonemalik@gmail.com>
2021-04-18 16:19:58 +0600

    App Name Change

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <div className='container' >
-      <Header title='Task Traker' onShow={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask} />
+      <Header title='Task Tracker' onShow={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0  ? 
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
